a73cc73c8e141915d7eac1b1bffcd983103cf640
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 21:41:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,8 +41,5 @@ new_music_artist_and_songs => {
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ 
 end
