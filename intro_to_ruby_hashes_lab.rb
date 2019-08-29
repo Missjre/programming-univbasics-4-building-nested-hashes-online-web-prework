9281722bd227e9b302751d0cde631a92cc144781
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 21:27:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,11 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  new_music_artist_and_songs ={ 
+    : music_artist => {
+      "Meg the Stallion" => {
+        : song => 
+        "Hot Girl Summer"
+        }
+	
 end
 
 def monopoly_with_second_tier
